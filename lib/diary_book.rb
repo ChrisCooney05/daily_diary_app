2a938041d77629b32b95e8df3958be01a54e377b
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-04 12:07:05 +0100

    logic added to add entry to database

--- lib/diary_book.rb
+++ lib/diary_book.rb
@@ -1,6 +1,15 @@
+require 'pg'
+
 class DiaryBook
 
   def self.add(entry)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'diary_entries_test')
+    else
+      connection = PG.connect(dbname: 'diary_entries')
+    end
 
+    connection.exec("INSERT INTO diary (entry) VALUES ('#{entry}');")
   end
+
 end
